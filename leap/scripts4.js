f3aa2746c25283df6214187c1d5c7ec58365b4f7
Micah Algard <micah@teamsodo.com>
2018-08-23 12:54:37 -0700

    updated triangle.js

--- leap/scripts4.js
+++ leap/scripts4.js
@@ -6,48 +6,38 @@ var Triangle = function (left, right, bottom) {
 
 Triangle.prototype.kind = function () {
     hyp = ([this.leftSide, this.rightSide, this.bottomSide]);
-    hyp.sort();
-    hyp.reverse();
-    return hyp;
+    hyp.sort(function(a, b){return b - a});
+    if(hyp[0] > hyp[1] + hyp[2]){
+        isTriangle = false;
+    }else{
+        isTriangle = true;
+    }
+    if(isTriangle) {
+        for(i=0; i<hyp.length; i++){
+            if(hyp[i] > 0){
+                isTriangle = true;
+                break;
+            }else{
+                isTriangle = false;
+            }
+        }
+    }
+    if (isTriangle){
+        if(this.leftSide == this.rightSide && this.rightSide == this.bottomSide){
+            return "equilateral"
+        }else if(this.leftSide == this.rightSide || this.leftSide == this.bottomSide || this.rightSide == this.bottomSide){
+            return "Isosceles";
+        }else if(this.leftSide != this.rightSide && this.rightSide != this.bottomSide ){
+            return "scalene"
+        }
+    }else{
+        return "not a triangle"
+    }
 }
 
-
-    let triangle = new Triangle(2, 11, 3);
-
-    console.log(triangle.kind());
+let triangle = new Triangle(2, 9, 0);
 
 
-    // if(this.leftSide > 0 && this.rightSide > 0 && this.bottomSide > 0){
-    //     tri = true;
-    // }else {
-    //     tri = false;
-    // }
-    // if(tri == true){
-    //     if(this.leftSide > this.rightSide && this.leftSide > this.buttonSide){
-    //         if(this.rightSide + this.buttonSide >= this.leftSide){
-    //             tri = true;
-    //         }else{
-    //             tri = false;
-    //         }
-    //     }
-    //     if(this.leftSide + this.rightSide >= this.bottomSide || this.bottomSide + this.leftSide >= this.rightSide || this.bottomSide + this.rightSide >= this.leftSide ){
-    //         tri = true;
-    //     }else{
-    //         tri = false;
-    //     }
-    // }
-    // return tri;
+console.log(triangle.kind())
 
-    // if (tri == true){
-    //     if(this.leftSide == this.rightSide && this.rightSide == this.bottomSide){
-    //         return "equilateral"
-    //     }else if(this.leftSide == this.rightSide || this.leftSide == this.bottomSide || this.rightSide == this.bottomSide){
-    //         return "Isosceles";
-    //     }else if(this.leftSide != this.rightSide && this.rightSide != this.bottomSide ){
-    //         return "scalene"
-    //     }else{
-    //         return "not a triangle "
-    //     }
-    // }else{
-    //     return "not a triangle"
-    // }
+
---
+++ triangle/triangle.js
@@ -0,0 +1,36 @@
+var Triangle = function (left, right, bottom) {
+    this.leftSide = left;
+    this.rightSide = right;
+    this.bottomSide = bottom;
+}
+
+Triangle.prototype.kind = function () {
+    hyp = ([this.leftSide, this.rightSide, this.bottomSide]);
+    hyp.sort(function(a, b){return b - a});
+    if(hyp[0] > hyp[1] + hyp[2]){
+        isTriangle = false;
+    }else{
+        isTriangle = true;
+    }
+    if(isTriangle) {
+        for(i=0; i<hyp.length; i++){
+            if(hyp[i] > 0){
+                isTriangle = true;
+                break;
+            }else{
+                isTriangle = false;
+            }
+        }
+    }
+    if (isTriangle){
+        if(this.leftSide == this.rightSide && this.rightSide == this.bottomSide){
+            return "equilateral"
+        }else if(this.leftSide == this.rightSide || this.leftSide == this.bottomSide || this.rightSide == this.bottomSide){
+            return "Isosceles";
+        }else if(this.leftSide != this.rightSide && this.rightSide != this.bottomSide ){
+            return "scalene"
+        }
+    }else{
+        return "not a triangle"
+    }
+}
